52c46d3d27fc600632092891c9832a9413c15476
gramcl <gmclellan3007@gmail.com>
2020-09-13 11:09:03 +1000

    First attempt at creating a single view page. Just copied everything over from the card component for now.

--- src/views/BeerShow.vue
+++ src/views/BeerShow.vue
@@ -1,28 +1,29 @@
 <template>
 
     <v-container>
-        <h1>Did I make it to individual page?</h1>
-        <p>Maybe....</p>
-        <p>The id is {{ id }} </p>
-
+        <BeerCard :beer="beer"></BeerCard> 
+        <!--<p> {{ beer.description  }} </p>-->
     </v-container>
 
 </template>
 
 <script>
 
+import BeerCard from "@/components/BeerCard.vue";
 import BeerService from "@/services/BeerService.js";
 
 export default {
     name: "BeerShow",
+    components: { BeerCard },
     props: ['id'],
     data: () => ({
         beer: {}
     }),
     created() {
         BeerService.getBeer(this.id).then( response => {
-            console.log(response)
-            this.beer = response.data
+            console.log(response.data[0])
+            this.beer = response.data[0]
+            console.log(this.beer)
         }).catch( error => {
         console.log(error);
         });
