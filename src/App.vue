7f21217e9c0151868cb859418be46d6b850cf8ae
gramcl <gmclellan3007@gmail.com>
2020-09-27 12:19:11 +1000

    Added a very basic btn and dialog to the nav bar. At the moment just the template code from the docs.

--- src/App.vue
+++ src/App.vue
@@ -10,9 +10,12 @@
           </v-col>
           <v-col cols="2" style="text-align: right;">
             <span class="px-4">
-              <router-link :to="{ name: 'BeerList'}">
+              <v-btn
+                 icon
+                @click.stop="dialog = true"
+               >
                 <v-icon>mdi-cart</v-icon>
-              </router-link>
+              </v-btn>
             </span>
             <span class="px-4">
               <router-link :to="{ name: 'BeerList'}">
@@ -20,6 +23,41 @@
               </router-link>
             </span>
           </v-col>
+
+
+          <v-dialog
+            v-model="dialog"
+            max-width="290"
+          >
+            <v-card>
+              <v-card-title class="headline">Use Google's location service?</v-card-title>
+
+              <v-card-text>
+                Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.
+              </v-card-text>
+
+              <v-card-actions>
+                <v-spacer></v-spacer>
+
+                <v-btn
+                  color="green darken-1"
+                  text
+                  @click="dialog = false"
+                >
+                  Disagree
+                </v-btn>
+
+                <v-btn
+                  color="green darken-1"
+                  text
+                  @click="dialog = false"
+                >
+                  Agree
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+
         </v-row>
       </v-container>
       
@@ -52,6 +90,7 @@ export default {
 
   data: () => ({
     //
+    dialog: false
   })
 };
 </script>
