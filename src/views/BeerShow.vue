cfe59de4235942db931542bd37abcc3018624eb5
gramcl <gmclellan3007@gmail.com>
2020-09-27 12:42:45 +1000

    Starting to add secondary nav bar for BeerShow component

--- src/views/BeerShow.vue
+++ src/views/BeerShow.vue
@@ -1,59 +1,65 @@
 <template>
+    <div>
+        <div class="secondary-nav">
+            <router-link :to="{ name: 'BeerList', query: { page: page } }">
+                <v-icon>mdi-chevron-left</v-icon>
+            </router-link>
+            <v-btn
+                icon
+                @click="addToList = true"
+               >
+                <v-icon>mdi-heart-outline</v-icon>
+            </v-btn>
+        </div>
+        <v-container>   
+        <v-row class="beer-card -shadow" >
+            <v-col class="beer-image" cols="12" sm="6">
+            <img :src="beer.image_url" :alt="beer.name" width="50">
+            </v-col>
+            <v-col cols="12" sm="6">
+            <p class="title"> {{ beer.name }} </p>
+            <p class="tagline"> {{ beer.tagline }} </p>
+            <p> ABV: {{ beer.abv }}% </p>
+            <p> {{ beer.description }}</p>
 
-    <v-container>
-
-      <v-row>
-          <router-link :to="{ name: 'BeerList', query: { page: page } }">
-            <v-icon>mdi-chevron-left</v-icon>
-          </router-link>
-      </v-row>
-      <v-row class="beer-card -shadow" >
-        <v-col class="beer-image" cols="12" sm="6">
-          <img :src="beer.image_url" :alt="beer.name" width="50">
-        </v-col>
-        <v-col cols="12" sm="6">
-          <p class="title"> {{ beer.name }} </p>
-          <p class="tagline"> {{ beer.tagline }} </p>
-          <p> ABV: {{ beer.abv }}% </p>
-          <p> {{ beer.description }}</p>
-
-          <div class="ingredients" v-if="beer.ingredients">
-            <p class="title">Ingredients:</p>
-            <template v-if="beer.ingredients.malt">
-                <p>Malt:</p>
-                <ul>
-                    <li v-for="malt in beer.ingredients.malt" :key="malt.id">
-                        {{ malt.name }}
-                    </li>
-                </ul>
-            </template>
-
-
-
-            <template v-if="beer.ingredients.hops">
-                <p>Hops:</p>
-                <ul>
-                    <li v-for="hops in beer.ingredients.hops" :key="hops.id">
-                        {{ hops.name }} - {{ hops.add }}
-                    </li>
-                </ul>
-            </template>
-
-            <template v-if="beer.ingredients.yeast">
-                <p>Yeast:</p>
-                <p> {{ beer.ingredients.yeast }} </p>
-            </template>
-
-          </div>
+            <div class="ingredients" v-if="beer.ingredients">
+                <p class="title">Ingredients:</p>
+                <template v-if="beer.ingredients.malt">
+                    <p>Malt:</p>
+                    <ul>
+                        <li v-for="malt in beer.ingredients.malt" :key="malt.id">
+                            {{ malt.name }}
+                        </li>
+                    </ul>
+                </template>
 
 
-        </v-col>
 
-        {{ this.page }}
-      </v-row>
+                <template v-if="beer.ingredients.hops">
+                    <p>Hops:</p>
+                    <ul>
+                        <li v-for="hops in beer.ingredients.hops" :key="hops.id">
+                            {{ hops.name }} - {{ hops.add }}
+                        </li>
+                    </ul>
+                </template>
 
-    </v-container>
+                <template v-if="beer.ingredients.yeast">
+                    <p>Yeast:</p>
+                    <p> {{ beer.ingredients.yeast }} </p>
+                </template>
 
+            </div>
+
+
+            </v-col>
+
+            {{ this.page }}
+        </v-row>
+
+        </v-container>
+
+    </div>
 </template>
 
 <script>
@@ -65,7 +71,8 @@ export default {
     name: "BeerShow",
     props: ['id', 'page'],
     data: () => ({
-        beer: {}
+        beer: {},
+        addToList: false
     }),
     created() {
         BeerService.getBeer(this.id).then( response => {
@@ -94,6 +101,9 @@ export default {
 }
 .beer-image {
     text-align: center;
+}
+.secondary-nav {
+    background-color: #CDCDCD;
 }
 
 </style>
