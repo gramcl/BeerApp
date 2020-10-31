83da781dbc71255f8e12c994535bbe7a10354cdb
gramcl <gmclellan3007@gmail.com>
2020-10-31 16:00:48 +1100

    Setting up form

--- src/views/OrderForm.vue
+++ src/views/OrderForm.vue
@@ -9,7 +9,7 @@
           <v-text-field
             v-model="lastname"
             :rules="nameRules"
-            :counter="10"
+            :counter="20"
             label="Last name"
             required
           ></v-text-field>
@@ -17,7 +17,7 @@
           <v-text-field
             v-model="firstname"
             :rules="nameRules"
-            :counter="10"
+            :counter="20"
             label="First name"
             required
           ></v-text-field>
@@ -29,11 +29,48 @@
             required
           ></v-text-field>
 
+          <div class="checkbox-grp d-flex justify-space-around">
+            <v-checkbox
+              v-model="interest"
+              value="beer"
+              label="Beer"
+            ></v-checkbox>
+
+            <v-checkbox
+              v-model="interest"
+              value="cider"
+              label="Cider"
+            ></v-checkbox>
+
+            <v-checkbox
+              v-model="interest"
+              value="wine"
+              label="Wine"
+            ></v-checkbox>
+          </div>
+
+          <v-radio-group
+              class="delivery-method"
+              v-model="method"
+              column
+              label="Postage Method:"
+            >
+            <v-radio
+              label="Delivery"
+              value="delivery"
+            ></v-radio>
+            <v-radio
+              label="Pickup"
+              value="pickup"
+            ></v-radio>
+
+          </v-radio-group>
+
           <div class="button-area">
             <v-btn
               class="mr-4"
               type="submit"
-              :disabled="invalid"
+              :disabled="!valid"
             >
               submit
             </v-btn>
@@ -51,6 +88,28 @@
 
 </template>
 
+<script>
+export default {
+  data: () => ({
+    valid: false,
+    interest: [],
+    method: '',
+    firstname: '',
+    lastname: '',
+    nameRules: [],
+    email: '',
+    emailRules: []
+
+  }),
+  methods: {
+    clear() {
+      return false;
+    }
+  }
+
+}
+</script>
+
 <style scoped>
   #main-form > h1{
     margin-top: 20px;
@@ -66,6 +125,10 @@
     width: 95%;
     margin-left: auto;
     margin-right: auto;
+  }
+
+  [role="radiogroup"] > .v-radio:first-of-type {
+    margin-top:20px;
   }
 
 @media (min-width: 420px){
